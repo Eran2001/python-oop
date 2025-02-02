103920526c14a475d707940d86d546846061f870
Eran <eran.hasarali.2001@gmail.com>
2025-02-02 09:09:33 +0530

    create simple blueprint and a instance of Employee class

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import re
-
-txt = "EraN"
-
-validation = re.search("^E.*N$", txt)
-
-if validation:
-  print("Yes")
-else:
-  print("No")
+class Employee:
+  def __init__(self, first, last, pay):
+    self.first = first
+    self.last = last
+    self.pay = pay
+    self.email = f"{first}.{last}@company.com"
+    
+  def full_name(self):
+    return "Hello, Mr.{} {}".format(self.first, self.last)
   
+emp_1 = Employee("Eran", "Hasareli", 50000)
+
+print(emp_1.full_name())
+print(Employee.full_name(emp_1))
